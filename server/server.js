ee7f0a3db18333ec80a41ce614915c20b43731ff
tonyap92 <114396404+tonyap92@users.noreply.github.com>
2024-12-12 19:36:52 +0300

    Changed the folder name.

--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const departmentRoutes = require('./routes/departments');
-const employeeRoutes = require('./routes/employees');
+const departmentRoutes = require('./api/departments');
+const employeeRoutes = require('./api/employees');
 
 dotenv.config();
 
